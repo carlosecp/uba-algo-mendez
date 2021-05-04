bbe67ebff0332ca52a1903feb00d668db74163e8
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-05-04 19:44:38 -0300

    I think I finished, gonna take suggestions

--- algo_mendez/tp_1/main.c
+++ algo_mendez/tp_1/main.c
@@ -26,10 +26,11 @@
 #define PTS_BAMBU 6
 #define PTS_PESCADO 9
 
-// Maximos
-#define MIN_INTERVALOS 1
+// Maximos y Minimos de Intervalos
 #define MAX_PISOS_TORRE 18
+#define MIN_PISOS_TORRE 1
 #define MAX_NIVEL_GRITO 18
+#define MIN_NIVEL_GRITO 1
 
 // Intervalos Resultados
 #define MIN_POLAR 5
@@ -45,21 +46,17 @@ bool es_alimento_valido(char alimento_ingresado) {
 	return (alimento_ingresado == BAMBU || alimento_ingresado == PESCADO || alimento_ingresado == FOCAS);
 }
 
-bool esta_incluido_en_intervalo(bool es_intervalo_cerrado, int cota_inferior, int cota_superior, int valor_a_evaluar) {
-	if (es_intervalo_cerrado)
-		return (cota_inferior <= valor_a_evaluar) && (valor_a_evaluar <= cota_superior);
-	else
-		return (cota_inferior < valor_a_evaluar) && (valor_a_evaluar < cota_superior);
+bool esta_incluido_en_intervalo(int cota_inferior, int cota_superior, int valor_a_evaluar) {
+	return (cota_inferior <= valor_a_evaluar) && (valor_a_evaluar <= cota_superior);
 }
 
 bool es_nivel_grito_valido(int nivel_grito) {
-	bool es_intervalo_cerrado = true;
-	return esta_incluido_en_intervalo(es_intervalo_cerrado, MIN_INTERVALOS, MAX_NIVEL_GRITO, nivel_grito);
+	return esta_incluido_en_intervalo(MIN_NIVEL_GRITO, MAX_NIVEL_GRITO, nivel_grito);
 }
 
 bool es_piso_torre_valido(int piso_torre) {
 	bool es_intervalo_cerrado = true;
-	return esta_incluido_en_intervalo(es_intervalo_cerrado, MIN_INTERVALOS, MAX_PISOS_TORRE, piso_torre);
+	return esta_incluido_en_intervalo(MIN_PISOS_TORRE, MAX_PISOS_TORRE, piso_torre);
 }
 
 int calcular_puntaje_segun_canal_tv(char canal_ingresado) {
@@ -97,7 +94,7 @@ int calcular_puntaje_segun_alimento(char alimento_ingresado) {
 int resultados_alimento() {
 	char alimento_ingresado;
 	do {
-		printf("Solo podes guardar un alimento en tu vianda - Bambú (B), Pescado (P), Focas (F):");
+		printf("-> Solo podes guardar un alimento en tu vianda - Bambú (B), Pescado (P), Focas (F): ");
 		scanf(" %c", &alimento_ingresado);
 	} while(!es_alimento_valido(alimento_ingresado));
 	return calcular_puntaje_segun_alimento(alimento_ingresado);
@@ -106,7 +103,7 @@ int resultados_alimento() {
 int resultados_canal_tv() {
 	char canal_ingresado;
 	do {
-		printf("Vas a ver televisión un rato, pones el canal de - Anime (A), Musica Pop (M), Limpieza (L): ");
+		printf("-> Vas a ver televisión un rato, pones el canal de - Anime (A), Musica Pop (M), Limpieza (L): ");
 		scanf(" %c", &canal_ingresado);
 	} while(!es_canal_tv_valido(canal_ingresado));
 	return calcular_puntaje_segun_canal_tv(canal_ingresado);
@@ -115,7 +112,7 @@ int resultados_canal_tv() {
 int resultados_nivel_grito() {
 	int nivel_grito_ingresado;
 	do {
-		printf("¡Oh, una rata! ¿Que tan fuerte gritas del 1 al 18? Siendo 1 no gritar y 18 desgarrarse la garganta: ");
+		printf("-> ¡Oh, una rata! ¿Que tan fuerte gritas del 1 al 18? Siendo 1 no gritar y 18 desgarrarse la garganta: ");
 		scanf(" %i", &nivel_grito_ingresado);
 	} while(!es_nivel_grito_valido(nivel_grito_ingresado));
 	return nivel_grito_ingresado;
@@ -124,7 +121,7 @@ int resultados_nivel_grito() {
 int resultados_piso_torre() {
 	int piso_torre_ingresado;
 	do {
-		printf("Te compras una torre con tus dos hermanos de 18 pisos. ¿En que piso te gustaría vivir?: ");
+		printf("-> Te compras una torre con tus dos hermanos de 18 pisos. ¿En que piso te gustaría vivir?: ");
 		scanf(" %i", &piso_torre_ingresado);
 	} while(!es_nivel_grito_valido(piso_torre_ingresado));
 	return piso_torre_ingresado;
@@ -135,31 +132,39 @@ int calcular_puntaje_total(int pts_segun_alimento, int pts_segun_canal_tv, int p
 }
 
 void asignar_personalidad(int pts_totales) {
-	char *descripcion_resultados;
 	char *personalidad_asignada;
+	char *descripcion_personalidad;
 	if (pts_totales >= MIN_POLAR && pts_totales <= MAX_POLAR)
 	{
-		descripcion_resultados = ("Podes ser el más jove, pero eso no te quita tu madurez.");
 		personalidad_asignada = "Polar";
+		descripcion_personalidad = ("Podes ser el más joven, pero eso no te quita tu madurez.");
 	}
 	else if (pts_totales <= MAX_PANDA)
 	{
-		descripcion_resultados = ("Parece que sos el mas tierno y delicado del trío.");
 		personalidad_asignada = "Panda";
+		descripcion_personalidad = ("Parece que sos el mas tierno y delicado del trío.");
 	}
 	else if (pts_totales <= MAX_PARDO)
 	{
-		descripcion_resultados = ("¿Cómo te va siendo el líder del trío?");
 		personalidad_asignada = "Pardo";
+		descripcion_personalidad = ("¿Cómo te va siendo el líder del trío?");
 	}
-	printf("Obtuviste %i puntos. Después de un arduo análisis, el súper test de personalidad determinó que tu personalidad escandalosa coincide con la de: — %s —", pts_totales, personalidad_asignada);
+	printf("\n*======================================================================*\n");
+	printf("\tPuntos Obtenidos: %i\n", pts_totales);
+	printf("\tTu personalidad escandaloza coincide con la de: %s\n", personalidad_asignada);
+	printf("\t%s\n", descripcion_personalidad);
+	printf("*======================================================================*");
 }
 
 int main() {
-	int pts_segun_alimento = resultados_alimento();
+	printf("*==================================================*\n");
+	printf("\tTest de Personalidad Escandaloza\n");
+	printf("*==================================================*\n\n");
+
 	int pts_segun_canal_tv = resultados_canal_tv();
-	int pts_nivel_grito = resultados_nivel_grito();
+	int pts_segun_alimento = resultados_alimento();
 	int pts_piso_torre = resultados_piso_torre();
+	int pts_nivel_grito = resultados_nivel_grito();
 
 	int pts_totales = calcular_puntaje_total(pts_segun_alimento, pts_segun_canal_tv, pts_nivel_grito, pts_piso_torre);
 	asignar_personalidad(pts_totales);
