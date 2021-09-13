0bf66bf4c7e83e5a9b74260cf5935760b328a85c
Carlos Castillo <itscarlosecp@gmail.com>
2021-09-12 21:22:22 -0300

    Changing to ubuntu

--- algo_2/tps/tp0/split.c
+++ algo_2/tps/tp0/split.c
@@ -1,4 +1,5 @@
 #include "split.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 #define END_OF_STRING '\0'
@@ -7,19 +8,13 @@ char** split(const char* string, char separador){
     size_t long_string = 0;
     size_t cant_substrings = 0;
 
-    for (; string[long_string] == END_OF_STRING; long_string++)
+    for (; string[long_string] != '\0'; long_string++)
         if (string[long_string] == separador) cant_substrings++;
 
-    char** strings = malloc(sizeof(char*) * cant_substrings + 1);
-
-    size_t long_substring = 0;
-    char* substring;
-    for (size_t i = 0, j = 0; i < long_string; i++) {
-        long_substring = 0;
-        while (string[i] != separador) long_substring++;
-        substring = malloc(sizeof(char) * long_substring + 1);
-    }
-
-    return NULL;
+    printf("Long: %li\nCant: %li", long_string, cant_substrings);
 }
 
+int main() {
+    split("testing:testing:testing", ':');
+    return 0;
+}
