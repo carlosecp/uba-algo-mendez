434436b2a3e393a06bfd040673a7b070adfe5635
Carlos Castillo <itscarlosecp@gmail.com>
2021-09-13 15:43:19 -0300

    Working

--- algo_2/tps/tp0/split.c
+++ algo_2/tps/tp0/split.c
@@ -3,26 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+void
+agregar_substring_a_vector(char** vector_strings, size_t long_substring) {
+	
+}
+
+int es_char_de_substring(char char_substring, char separador) {
+	return char_substring != separador && char_substring != '\0';
+}
+
 char** split(const char* string, char separador){
-	if (string == NULL) return NULL;
+	if (!string) return NULL;
 
 	size_t long_string = 0;
 	size_t cant_substrings = 0;
 
-	for (; string[long_string]; long_string++) {
+	for (; string[long_string]; long_string++)
 		if (string[long_string] == separador) cant_substrings++;
-		printf("string[%li]: %c\n", long_string, string[long_string]);
-	}
 
-	if (cant_substrings) cant_substrings++;
-	printf("cant_substrings: %li\n", cant_substrings);
+	cant_substrings++;
 
 	char** vector_strings = malloc((cant_substrings + 1) * sizeof(char*));
-
 	size_t long_substring = 0;
+
 	for (size_t i = 0; i < cant_substrings; i++) {
 		long_substring = 0;
-		while (string[long_substring] != separador && string[long_substring] != '\0')
+		while (es_char_de_substring(string[long_substring], separador))
 			long_substring++;
 
 		vector_strings[i] = malloc((long_substring + 1) * sizeof(char));
@@ -34,22 +40,5 @@ char** split(const char* string, char separador){
 	}
 
 	vector_strings[cant_substrings] = NULL;
-
 	return vector_strings;
-}
-
-void imprimir_strings(char* vector_strings[]) {
-	for (size_t i = 0; vector_strings[i]; i++) {
-		for (size_t j = 0; vector_strings[i][j] != '\0'; j++)
-			printf("vector_strings[%li]: %c", i, vector_strings[i][j]);
-		printf("\n");
-	}
-}
-
-int main() {
-	char* string = ",";
-	char** vector_strings = split(string, ',');
-	imprimir_strings(vector_strings);
-	free(vector_strings);
-	return 0;
 }
