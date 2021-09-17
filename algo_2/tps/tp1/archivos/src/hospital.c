f3e5f7164cb52f5ce5a4968aedef863bd113fc1e
Carlos Castillo <itscarlosecp@gmail.com>
2021-09-17 20:49:11 -0300

    Terminado

--- algo_2/tps/tp1/archivos/src/hospital.c
+++ algo_2/tps/tp1/archivos/src/hospital.c
@@ -3,49 +3,59 @@
 #include "split.h"
 
 struct _hospital_pkm_t{
-    size_t cantidad_pokemon;
-    pokemon_t* vector_pokemones;
+	size_t cantidad_pokemon;
+	pokemon_t* vector_pokemones;
 };
 
 struct _pkm_t{
-    char* nombre;
-    size_t nivel;
+	char* nombre;
+	size_t nivel;
 };
 
 hospital_t* hospital_crear(){
-    return calloc(1, sizeof(hospital_t));
+	return calloc(1, sizeof(hospital_t));
 }
 
-bool hospital_leer_archivo(hospital_t* hospital, const char* nombre_archivo){
-	if (!hospital) return false;
-
+bool
+hospital_leer_archivo(hospital_t* hospital, const char* nombre_archivo){
 	FILE* archivo = fopen(nombre_archivo, "r");
-	if (!archivo)
+	if (!hospital || !archivo)
 		return false;
 
 	return true;
 }
 
-size_t hospital_cantidad_pokemon(hospital_t* hospital){
-    return 0;
+size_t
+hospital_cantidad_pokemon(hospital_t* hospital){
+	return 0;
 }
 
-size_t hospital_cantidad_entrenadores(hospital_t* hospital){
-    return 0;
+size_t
+hospital_cantidad_entrenadores(hospital_t* hospital){
+	return 0;
 }
 
-size_t hospital_a_cada_pokemon(hospital_t* hospital, bool (*funcion)(pokemon_t* p)){
-    return 0;
+size_t
+hospital_a_cada_pokemon(hospital_t* hospital, bool (*funcion)(pokemon_t* p)){
+	return 0;
 }
 
-void hospital_destruir(hospital_t* hospital){
-
+void
+hospital_destruir(hospital_t* hospital){
+	size_t cantidad_pokemones = hospital_cantidad_pokemon(hospital);
+	for (size_t i = 0; i < cantidad_pokemones; i++) {
+		free(hospital -> vector_pokemones[i] . nombre);
+	}
+	free(hospital -> vector_pokemones);
+	free(hospital);
 }
 
-size_t pokemon_nivel(pokemon_t* pokemon){
-    return 0;
+size_t
+pokemon_nivel(pokemon_t* pokemon){
+	return 0;
 }
 
-const char* pokemon_nombre(pokemon_t* pokemon){
-    return NULL;
+const char*
+pokemon_nombre(pokemon_t* pokemon){
+	return NULL;
 }
