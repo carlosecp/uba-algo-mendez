670bf49da58705eaa2e689490e1bbe6390d09321
Carlos Castillo <itscarlosecp@gmail.com>
2021-10-08 21:05:49 -0300

    Macro de tipos

--- algo_2/tps/tda1/pruebas.c
+++ algo_2/tps/tda1/pruebas.c
@@ -6,6 +6,7 @@
 #include "pa2mm.h"
 
 /* PRUEBAS DE LISTA */
+/* CREACION */
 
 void
 alCrearUnaLista_seCreaExitosamente()
@@ -15,9 +16,32 @@ alCrearUnaLista_seCreaExitosamente()
 	pa2m_afirmar(lista != NULL, "La lista se crea exitosamente");
 	pa2m_afirmar(lista -> nodo_inicio == NULL, "El nodo de inicio de la lista es igual a NULL");
 	pa2m_afirmar(lista -> nodo_fin == NULL, "El nodo de fin de la lista es igual a NULL");
+	pa2m_afirmar(lista -> cantidad == 0, "La cantidad de nodos igual a 0");
 
 	lista_destruir(lista);
 }
+
+/* INSERCION */
+
+void
+dadaUnaLista_alInsertarUnElemento_retornaLaLista()
+{
+	lista_t* lista = lista_crear();
+
+	int elemento = 0;
+	pa2m_afirmar(lista_insertar(lista, &elemento), "Al insertar un nodo exitosamente");
+
+	lista_destruir(lista);
+}
+
+void
+dadaUnaListaNULL_alInsertarUnElemento_retornaNULL()
+{
+	lista_t* lista = lista_crear();
+
+	lista_destruir();
+}
+
 
 int
 main()
